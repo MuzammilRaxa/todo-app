9463ca64d80eabcb2657590baac29267dc1d31d5
[razamuzammil426@gmail.com] <razamuzammil426@gmail.com>
2022-12-29 11:32:13 +0500

    JS-programs-Demos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import List  from './components/List'
+// import List  from './components/List'
 
-function App() {
-  return (
-    <div className="App">
-      < List />
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       < List />
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
--- src/components/List.js
+++ src/components/List.js
@@ -1,45 +1,45 @@
-import React, { useState } from 'react'
+// import React, { useState } from 'react'
 
 
 
-function List() {
+// function List() {
 
-    const [tasks, setTasks] = useState(['eatinggg', 'sleeping'])
-    const [task, setTask] = useState('')
-
-
-    const deleteTask = (i) => {
-        const newTask = [...tasks];
-        newTask.splice(i, 1)
-        setTasks(newTask)
-    }
-
-    const allTasks = () => {
-        return tasks.map((task, index) => <li onClick={() => { deleteTask(index) }} className="list-group-item">{task}</li>)
-    }
+//     const [tasks, setTasks] = useState(['eatinggg', 'sleeping'])
+//     const [task, setTask] = useState('')
 
 
-    const addTask = (addNewTask) => {
-        if (task) {
-            const newTask = [...tasks];
-            newTask.push(addNewTask)
-            setTasks(newTask)
-            setTask('');
-        }
-        else { alert('type valid value') }
-    }
+//     const deleteTask = (i) => {
+//         const newTask = [...tasks];
+//         newTask.splice(i, 1)
+//         setTasks(newTask)
+//     }
 
-    return (
-        <div>
-            <div className='container'>
-                <input className='form-control' placeholder={'type your list'} value={task} onChange={(e) => { setTask(e.target.value) }} />
-                <button className={'btn btn-success w-100'} onClick={() => addTask(task)} >Add Task</button>
-                <ul className="list-group">
-                    {allTasks()}
-                </ul>
-            </div>
-        </div>
-    )
-}
+//     const allTasks = () => {
+//         return tasks.map((task, index) => <li onClick={() => { deleteTask(index) }} className="list-group-item">{task}</li>)
+//     }
 
-export default List
+
+//     const addTask = (addNewTask) => {
+//         if (task) {
+//             const newTask = [...tasks];
+//             newTask.push(addNewTask)
+//             setTasks(newTask)
+//             setTask('');
+//         }
+//         else { alert('type valid value') }
+//     }
+
+//     return (
+//         <div>
+//             <div className='container'>
+//                 <input className='form-control' placeholder={'type your list'} value={task} onChange={(e) => { setTask(e.target.value) }} />
+//                 <button className={'btn btn-success w-100'} onClick={() => addTask(task)} >Add Task</button>
+//                 <ul className="list-group">
+//                     {allTasks()}
+//                 </ul>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default List
